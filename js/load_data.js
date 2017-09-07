ed32d5db9fca0b252ccad6df266990a3598fd4b8
Peter M <aaa@aaa.sk>
2017-09-07 11:45:29 +0200

    Sort in table

--- js/load_data.js
+++ js/load_data.js
@@ -17,6 +17,7 @@ console.log('Click');
             var input =client.responseText;
              //console.log(input);
              showResult(input);
+            
              
              
           }
@@ -43,10 +44,25 @@ function showResult(xmlhttp) {
      var table = document.createElement("table");
     var tr = table.insertRow(-1);              
 
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");     
+        for (var i = 0; i < col.length; i++) {   
+            if(col[i]=="name" || col[i]=="population"){
+            // console.log(col[i]);
+              var th = document.createElement("th");
+            th.innerHTML = col[i]+'   <button id="but" onclick="sortTable('+i+')" ><i class="fa fa-sort-desc"></button>';
+          
+            tr.appendChild(th);  
+           /* th.onclick = function () {
+           console.log(i);   
+     sortTable(i);
+       }; */
+            }
+            else{
+            var th = document.createElement("th");
             th.innerHTML = col[i];
             tr.appendChild(th);
+            
+            }    
+            
         }
 
      
@@ -66,4 +82,58 @@ function showResult(xmlhttp) {
         divContainer.appendChild(table);
     
 
+}
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("showData");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("tr");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("td")[n];
+      y = rows[i + 1].getElementsByTagName("td")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
